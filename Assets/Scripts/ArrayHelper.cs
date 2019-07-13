ad5c4c129ebfcb562b0c3e7624bafda25b78021e
Valeriy <stratulatmark@yandex.ru>
2019-07-13 21:29:06 +0300

    Small refactoring

--- Assets/Scripts/ArrayHelper.cs
+++ Assets/Scripts/ArrayHelper.cs
@@ -3,42 +3,11 @@ public struct ArrayHelper
     public int Width;
     public int Height;
         
-    public int GetRight(int i)
-    {
-        if (i % Width < Width - 1) return i + 1;
-        return -1;
-    }
-            
-    public int GetLeft(int i)
-    {
-        if (i % Width > 0) return i - 1;
-        return -1;
-    }
-
-    public int GetUp(int i)
-    {
-        if ((i += Width) >= Width * Height) return -1;
-        return i;
-    }
-
-    public int GetDown(int i)
-    {
-        if ((i -= Width) < 0) return -1;
-        return i;
-    }
-
-    public int GetX(int i)
-    {
-        return i % Width;
-    }
-
-    public int GetY(int i)
-    {
-        return i / Width;
-    }
-
-    public int GetI(int x, int y)
-    {
-        return y * Width + x;
-    }
+    public int GetRight(int i) => (i % Width < Width - 1) ? i + 1 : -1;
+    public int GetLeft(int i) => (i % Width > 0) ? i - 1 : -1;
+    public int GetUp(int i) => ((i += Width) < Width * Height) ? i : -1;
+    public int GetDown(int i) => ((i -= Width) >= 0) ? i : -1;
+    public int GetX(int i) => i % Width;
+    public int GetY(int i) => i / Width;
+    public int GetI(int x, int y) => y * Width + x;
 }
